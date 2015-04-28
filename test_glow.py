decf5e8e5692719aacf03b515dec229295b9c217
scivision <scivision@users.noreply.github.com>
2015-04-28 01:46:26 -0400

    gnu90

--- test_glow.py
+++ test_glow.py
@@ -3,10 +3,10 @@
 Registration testing of GLOW
 Michael Hirsch
 """
-from numpy import array,gradient
+from numpy import array,tile,roots
 from numpy.testing import assert_allclose
 #%% test inputs
-nbins = 190
+nbins = 190; jmax=120 #glow.h
 eflux = 1.
 e0 = 1e3
 maxind = 112
@@ -18,3 +18,8 @@ assert_allclose(ener[[maxind,maxind+10,-1]],[1017.7124,1677.9241,47825.418])
 phi = maxt(eflux,e0,ener, dE, itail=0, fmono=0, emono=0)
 assert phi.argmax() == maxind
 assert_allclose(phi[[maxind,maxind+10]],[ 114810.6,97814.438])
+#%% test vquart (quartic root)
+from aurora import vquartmod
+Aquart = tile([-1,0,0,0,1],(jmax,1))
+qroot = vquartmod(Aquart,1)
+assert_allclose(qroot[0],roots(Aquart[0,-1]))
