8b368cb25625589aed94d981a6e5baa881c75b89
scivision <scivision@users.noreply.github.com>
2015-09-06 23:32:38 -0400

    v0.973 reference

---
+++ reference/dayexample.f
@@ -0,0 +1,266 @@
+C Example driver program for GLOW subroutine package - dayglow version.
+C
+C Stan Solomon, 4/05, 12/14
+C
+C This software is part of the GLOW model.  Use is governed by the Open Source
+C Academic Research License Agreement contained in the file glowlicense.txt.
+C
+C For more information see the file glow.txt
+C
+C For definitions of common block /CGLOW/, see subroutine GLOW
+C
+C Other definitions:
+C F107P   Solar 10.7 cm flux for previous day
+C AP      Ap index of geomagnetic activity
+C ICONJ   conjugate photoelectron flux switch: 0=off, 1=on
+C Z       altitude array, km
+C XNO     default values for NO concentration, cm-3
+C
+C Array dimensions:
+C JMAX    number of altitude levels
+C NBINS   number of energetic electron energy bins
+C LMAX    number of wavelength intervals for solar flux
+C NMAJ    number of major species
+C NEX     number of ionized/excited species
+C NW      number of airglow emission wavelengths
+C NC      number of component production terms for each emission
+C NST     number of states produced by photoionization/dissociation
+C NEI     number of states produced by electron impact
+C NF      number of types of auroral fluxes
+C
+      INCLUDE 'glow.h'
+      PARAMETER (NMAJ=3)
+      PARAMETER (NEX=20)
+      PARAMETER (NW=20)
+      PARAMETER (NC=10)
+      PARAMETER (NST=6)
+      PARAMETER (NEI=10)
+      PARAMETER (NF=4)
+C
+      COMMON /CGLOW/
+     >    IDATE, UT, GLAT, GLONG, ISCALE, JLOCAL, KCHEM,
+     >    F107, F107A, HLYBR, FEXVIR, HLYA, HEIEW, XUVFAC,
+     >    ZZ(JMAX), ZO(JMAX), ZN2(JMAX), ZO2(JMAX), ZNO(JMAX),
+     >    ZNS(JMAX), ZND(JMAX), ZRHO(JMAX), ZE(JMAX),
+     >    ZTN(JMAX), ZTI(JMAX), ZTE(JMAX),
+     >    PHITOP(NBINS), EFLUX(NF), EZERO(NF),
+     >    SZA, DIP, EFRAC, IERR,
+     >    ZMAJ(NMAJ,JMAX), ZCOL(NMAJ,JMAX),
+     >    WAVE1(LMAX), WAVE2(LMAX), SFLUX(LMAX),
+     >    ENER(NBINS), DEL(NBINS),
+     >    PESPEC(NBINS,JMAX), SESPEC(NBINS,JMAX),
+     >    PHOTOI(NST,NMAJ,JMAX), PHOTOD(NST,NMAJ,JMAX), PHONO(NST,JMAX),
+     >    QTI(JMAX), AURI(NMAJ,JMAX), PIA(NMAJ,JMAX), SION(NMAJ,JMAX),
+     >    UFLX(NBINS,JMAX), DFLX(NBINS,JMAX), AGLW(NEI,NMAJ,JMAX),
+     >    EHEAT(JMAX), TEZ(JMAX), ECALC(JMAX),
+     >    ZXDEN(NEX,JMAX), ZETA(NW,JMAX), ZCETA(NC,NW,JMAX), VCB(NW)
+C
+      COMMON /CXSECT/ SIGS(NMAJ,NBINS), PE(NMAJ,NBINS), PIN(NMAJ,NBINS),
+     >                SIGA(NMAJ,NBINS,NBINS), SEC(NMAJ,NBINS,NBINS),
+     >                SIGEX(NEI,NMAJ,NBINS), SIGIX(NEI,NMAJ,NBINS),
+     >                IIMAXX(NBINS)
+C
+      DIMENSION Z(JMAX), D(8), T(2), SW(25),
+     >          OUTF(11,JMAX), OARR(30), TPI(NMAJ)
+C
+      LOGICAL JF(12)
+C
+      DATA SW/25*1./
+      DATA PI/3.1415926536/
+      DATA Z/     80., 81., 82., 83., 84., 85., 86., 87., 88., 89.,
+     >            90., 91., 92., 93., 94., 95., 96., 97., 98., 99.,
+     >           100.,101.,102.,103.,104.,105.,106.,107.,108.,109.,
+     >           110.,111.5,113.,114.5,116.,118.,120.,122.,124.,126.,
+     >           128.,130.,132.,134.,136.,138.,140.,142.,144.,146.,
+     >           148.,150.,153.,156.,159.,162.,165.,168.,172.,176.,
+     >           180.,185.,190.,195.,200.,205.,211.,217.,223.,230.,
+     >           237.,244.,252.,260.,268.,276.,284.,292.,300.,309.,
+     >           318.,327.,336.,345.,355.,365.,375.,385.,395.,406.,
+     >           417.,428.,440.,453.,467.,482.,498.,515.,533.,551.,
+     >           570.,590.,610.,630.,650.,670.,690.,710.,730.,750.,
+     >           770.,790.,810.,830.,850.,870.,890.,910.,930.,950./
+C
+C
+C Obtain input parameters:
+C
+      read (5,*) idate,ut,glat,glong,f107a,f107,f107p,ap
+C
+C Set other parameters and switches:
+C
+      JLOCAL = 0
+      KCHEM = 4
+      ISCALE = 1
+      XUVFAC = 3.
+      ICONJ = 1
+      HLYBR = 0.
+      FEXVIR = 0.
+      HLYA = 0.
+      HEIEW = 0.
+C
+C
+C Calculate local solar time:
+C
+      STL = (UT/240.+GLONG) / 15.
+      IF (STL .LT. 0.) STL = STL + 24.
+      IF (STL .GT. 24.) STL = STL - 24.
+C
+C
+C Call MSIS-2K to get neutral densities and temperature:
+C
+      CALL TSELEC(SW)
+C
+        DO J=1,JMAX
+          CALL GTD7(IDATE,UT,Z(J),GLAT,GLONG,STL,F107A,F107P,AP,48,D,T)
+          ZO(J) = D(2)
+          ZN2(J) = D(3)
+          ZO2(J) = D(4)
+          ZRHO(J) = D(6)
+          ZNS(J) = D(8)
+          ZTN(J) = T(2)
+        END DO
+C
+C
+C Call SNOEMINT to obtain NO profile from the Nitric Oxide Empirical
+C Model (NOEM)
+C
+      CALL SNOEMINT(IDATE,GLAT,GLONG,F107,AP,JMAX,Z,ZTN,ZNO)
+C
+C
+C Call International Reference Ionosphere-1990 subroutine to get
+C electron density and temperature and ion temperature:
+C
+C NOTE: the directory specified in the call to IRI90 must be changed
+C to the one where the ccirnn.asc and ursinn.asc files are.
+C
+      DO IJF=1,12
+        JF(IJF) = .TRUE.
+      END DO
+      JF(5) = .FALSE.
+      JMAG = 0
+      RZ12 = -F107A
+      IDAY = IDATE - IDATE/1000*1000
+      MMDD = -IDAY
+      CALL IRI90(JF,JMAG,GLAT,GLONG,RZ12,MMDD,STL,Z,JMAX,
+     >           '/home/stans/mod/iri/',OUTF,OARR)
+      DO J=1,JMAX
+        ZE(J) = OUTF(1,J) / 1.E6
+        IF (ZE(J) .LT. 100.) ZE(J) = 100.
+        ZTI(J) = OUTF(3,J)
+        IF (ZTI(J) .LT. ZTN(J)) ZTI(J) = ZTN(J)
+        ZTE(J) = OUTF(4,J)
+        IF (ZTE(J) .LT. ZTN(J)) ZTE(J) = ZTN(J)
+        ZXDEN(3,J) = ZE(J) * OUTF(5,J)/100.
+        ZXDEN(6,J) = ZE(J) * OUTF(8,J)/100.
+        ZXDEN(7,J) = ZE(J) * OUTF(9,J)/100.
+      END DO
+C
+C
+C Fill altitude array and initialize and N(2D):
+C
+      DO J=1,JMAX
+        ZZ(J)   = Z(J) * 1.E5
+        ZND(J)  = 0.
+      END DO
+C
+C
+C Put zero electron flux into PHITOP array:
+C
+      DO N=1,NBINS
+        PHITOP(N) = 0.0
+      END DO
+C
+C
+C Call GLOW to calculate ionized and excited species, airglow emission
+C rates, and vertical column brightnesses:
+C
+      CALL GLOW
+C
+C
+C Find location of conjugate point:
+C
+      GLATS = GLAT
+      GLONGS = GLONG
+      CALL GEOMAG(0,GLONG,GLAT,XMLONG,XMLAT)
+      CALL GEOMAG(1,GLONG,GLAT,XMLONG,-XMLAT)
+C
+C
+C Downward flux at conjugate point = upward flux at location:
+C
+      DO N=1,NBINS
+        PHITOP(N) = UFLX(N,JMAX)
+      END DO
+C
+C
+C Call Glow for conjugate point (same neutral atmosphere):
+C
+      IF (ICONJ .EQ. 1) CALL GLOW
+C
+C
+C Upward flux at conjugate point = downward flux at location:
+C
+      DO N=1,NBINS
+        PHITOP(N) = UFLX(N,JMAX)
+      END DO
+C
+C
+C Call Glow again at location:
+C
+      SZAC = SZA
+      DIPC = DIP
+      GLATC = GLAT
+      GLONGC = GLONG
+      GLAT = GLATS
+      GLONG = GLONGS
+C
+      IF (ICONJ .EQ. 1) CALL GLOW
+C
+C
+C Output section:
+C
+      SZAD = SZA * 180. / PI
+      DIPD = DIP * 180. / PI
+      SZACD = SZAC * 180. / PI
+      DIPCD = DIPC * 180. / PI
+      write (6,444) IDATE, UT, GLAT, GLONG, F107, F107A, AP
+  444 FORMAT (' Date=',i5,' UT=',f6.0,' Lat=',f5.1,' Lon=',f6.1,
+     >        ' F107=',f4.0,' F107A=',f4.0,' Ap=',f4.0)
+      WRITE (6,445) SZAD, STL, DIPD, GLATC, GLONGC, SZACD, EFRAC, IERR
+  445 FORMAT (' SZA=',F5.1,' LST=',F5.2,' Dip=',F5.1,' Clat=',F5.1,
+     >        ' Clon=',F6.1,' CSZA=',F5.1,' Ec=',F6.3,' Ie=',I1)
+C
+C
+C Output photoionization, electron impact ionization,
+C electron density, and ion densities:
+C
+      write (6,690)
+  690 format ('  Z     Photoion   EIion    Ecalc     O+(2P)    ',
+     >        'O+(2D)    O+(4S)     N+         N2+       O2+       NO+')
+      do 750 j=1,jmax
+        do 700 i=1,nmaj
+          tpi(i) = 0.
+          do 700 ns=1,nst
+            tpi(i) = tpi(i) + photoi(ns,i,j)
+  700     continue
+        totpi = tpi(1) + tpi(2) + tpi(3) + phono(1,j)
+        totsi = sion(1,j) + sion(2,j) + sion(3,j)
+        write (6,730) z(j),totpi,totsi,ecalc(j),(zxden(i,j),i=1,7)
+  730   format (1x, 0p, f5.1, 1p, 14e10.2)
+  750 continue
+C
+C
+C Output selected volume emission rates and column brightnesses:
+C
+      write (6,780)
+  780 format ('   z     3371   4278   5200   5577   6300',
+     >        '   7320  10400   3466   7774   8446')
+      write (6,790) (z(j), (zeta(iw,j),iw=1,10), j=1,jmax)
+  790 format (1x, f5.1, 10f7.1)
+      write (6,795)  (vcb(iw),iw=1,10)
+  795 format (' VCB:',11f7.0)
+
+C
+C
+C     CALL ROUT('rt.out',13,EF,EZ,ITAIL,FRACO,FRACO2,FRACN2)
+C
+      STOP
+      END
---
+++ reference/rout.f
@@ -0,0 +1,90 @@
+C Subroutine ROUT writes model atmosphere and excitation rates to an
+C output file in order to transfer them to radiative transfer program.
+C
+C This software is part of the GLOW model.  Use is governed by the Open Source
+C Academic Research License Agreement contained in the file glowlicense.txt.
+C For more information see the file glow.txt.
+C
+C SMB/SCS, 9/94
+C Replaced 834 with LBH, SCS, 2/03
+C Reduced cascade contribution to 1356, SCS, 9/03
+C Included radiative recombination in 1356 SCS, 9/03
+C
+      SUBROUTINE ROUT(ROFILE,LUN,EF,EZ,ITAIL,FRACO,FRACO2,FRACN2)
+
+      INCLUDE 'glow.h'
+      PARAMETER (NMAJ=3)
+      PARAMETER (NEX=20)
+      PARAMETER (NW=20)
+      PARAMETER (NC=10)
+      PARAMETER (NST=6)
+      PARAMETER (NEI=10)
+      PARAMETER (NF=4)
+C
+      COMMON /CGLOW/
+     >    IDATE, UT, GLAT, GLONG, ISCALE, JLOCAL, KCHEM,
+     >    F107, F107A, HLYBR, FEXVIR, HLYA, HEIEW, XUVFAC,
+     >    ZZ(JMAX), ZO(JMAX), ZN2(JMAX), ZO2(JMAX), ZNO(JMAX),
+     >    ZNS(JMAX), ZND(JMAX), ZRHO(JMAX), ZE(JMAX),
+     >    ZTN(JMAX), ZTI(JMAX), ZTE(JMAX),
+     >    PHITOP(NBINS), EFLUX(NF), EZERO(NF),
+     >    SZA, DIP, EFRAC, IERR,
+     >    ZMAJ(NMAJ,JMAX), ZCOL(NMAJ,JMAX),
+     >    WAVE1(LMAX), WAVE2(LMAX), SFLUX(LMAX),
+     >    ENER(NBINS), DEL(NBINS),
+     >    PESPEC(NBINS,JMAX), SESPEC(NBINS,JMAX),
+     >    PHOTOI(NST,NMAJ,JMAX), PHOTOD(NST,NMAJ,JMAX), PHONO(NST,JMAX),
+     >    QTI(JMAX), AURI(NMAJ,JMAX), PIA(NMAJ,JMAX), SION(NMAJ,JMAX),
+     >    UFLX(NBINS,JMAX), DFLX(NBINS,JMAX), AGLW(NEI,NMAJ,JMAX),
+     >    EHEAT(JMAX), TEZ(JMAX), ECALC(JMAX),
+     >    ZXDEN(NEX,JMAX), ZETA(NW,JMAX), ZCETA(NC,NW,JMAX), VCB(NW)
+C
+      dimension z(jmax), zhe(jmax), e1356(jmax), e1304(jmax),
+     >          e1027(jmax), e989(jmax), elbh(jmax)
+      character*40 rofile
+C
+      do 50 j=1,jmax
+      z(j)=zz(j)/1.e5
+      zhe(j)=0.
+C Temporary fix-up to 1356:
+      e1356(j)=aglw(3,1,j)+aglw(5,1,j)*0.7+ecalc(j)*zxden(3,j)*4.9e-13
+      e1304(j)=aglw(4,1,j)+aglw(6,1,j)+aglw(7,1,j)+aglw(8,1,j)*0.1
+      e1027(j)=aglw(7,1,j)
+      e989(j)=aglw(8,1,j)
+      elbh(j)=aglw(4,3,j)
+  50  continue
+C
+      open(unit=lun,file=rofile,status='unknown')
+C  
+      write(lun,100)
+ 100  format('   JMAX ','   SZA  ','   UT   ','   IDATE',
+     >             '   LAT  ','   LONG ','    DIP ')
+      write(lun,200) jmax,sza*180./3.14159,ut,idate,glat,glong,dip
+ 200  format(i8,f8.2,f8.1,i8,3f8.2)
+      write(lun,250)
+ 250  format('  F107  ','  F107p ','  F107a ','  HLyBr ',
+     >       ' FeXVIr ','   HLYa ','  HeIew ',' XUVfac ')
+      write(lun,300) f107,f107p,f107a,hlybr,fexvir,hlya,heiew,xuvfac
+ 300  format (5f8.2,1pe10.2,0p2f8.2)
+      write(lun,350)
+ 350  format(' Eflux  ',' Ezero  ',' Itail  ',
+     >       ' FracO  ',' FracO2  ',' FracN2  ')
+      write(lun,400) ef, ez, itail, fraco, fraco2, fracn2
+ 400  format(f8.2,f8.1,i8,3f8.2)
+      write(lun,500)
+ 500  format(' Alt    Tn    Ti    Te  ',
+     >       '    O        O2       N2       He   ',
+     >       '    N        Ne       O+      1356  ',
+     >       '   1304     1027      989     LBH')
+C
+      do 700,j=1,jmax
+        write(lun,600) z(j),ztn(j),zti(j),zte(j),
+     >               zo(j),zo2(j),zn2(j),zhe(j),
+     >               zns(j),ecalc(j),zxden(3,j),e1356(j),
+     >               e1304(j),e1027(j),e989(j),elbh(j)
+ 600    format(0p,f6.1,3f6.0,1p,12e9.2)
+ 700  continue
+C
+      close(lun)
+      return
+      end
