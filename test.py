755744373025ce4d55207924a698afea5076859a
michael <hirsch617@gmail.com>
2015-09-09 01:44:07 -0400

    pass of legacy test

--- test.py
+++ test.py
@@ -28,13 +28,14 @@ nmaj=3; nst=6
 dtime = datetime(2013,4,14,8,54,0)
 #
 yd,utsec = datetime2yd(dtime)[:2]
-#%% test of egrid
-ener,dE = aurora.egrid()
-assert_allclose(ener[[maxind,maxind+10,-1]],[1017.7124,1677.9241,47825.418])
+
+def test_egrid_maxt():
+    ener,dE = aurora.egrid()
+    assert_allclose(ener[[maxind,maxind+10,-1]],[1017.7124,1677.9241,47825.418])
 #%% test of maxt
-phi = aurora.maxt(eflux,e0,ener, dE, itail=0, fmono=0, emono=0)
-assert phi.argmax() == maxind
-assert_allclose(phi[[maxind,maxind+10]],[ 114810.6,97814.438])
+    phi = aurora.maxt(eflux,e0,ener, dE, itail=0, fmono=0, emono=0)
+    assert phi.argmax() == maxind
+    assert_allclose(phi[[maxind,maxind+10]],[ 114810.6,97814.438])
 
 #%% test vquart (quartic root) KNOWN DEFECTIVE FORTRAN ALGORITHM
 #Aquart = tile([-1,0,0,0,1],(jmax,1))
@@ -51,49 +52,61 @@ assert_allclose(phi[[maxind,maxind+10]],[ 114810.6,97814.438])
 #except AssertionError as e:
 #    print('this mismatch is in discussion with S. Solomon.   {}'.format(e))
 
-#%% test snoem
-doy = datetime2gtd(dtime)[0]
-zno,maglat,nozm = aurora.snoemmod.snoem(doy,1.75*log(0.4*ap),f107)
-assert_allclose((nozm[12,15],nozm[-2,-1]),(33547142.,  44171752.))
-#%% test snoemint
-densd,tempd = rungtd1d(dtime,z,glat,glon,f107a,f107,[ap]*7)
-znoint = aurora.snoemint(dtime.strftime('%Y%j'),glat,glon,f107,ap,z,tempd['heretemp'])
-assert_allclose(znoint[[28,63]], (1.40939280e+08,   4.11383025e+06))
-#%% test fieldm
-xdip,ydip,zdip,totfield,dipang,decl,smodip = aurora.fieldm(glat,glon%360,z[50])
-assert isclose(xdip,0.10698765516281128)
-assert isclose(totfield,0.532055139541626)
-assert isclose(dipang,76.86974334716797)
-#%% test solzen
-sza = aurora.szacalc(yd,utsec,glat,glon)
-assert isclose(sza, 104.68412017822266)
-#%% test ssflux
-iscale=1; hlybr=0.; hlya=0.; fexvir=0.; heiew=0.; xuvfac=3.
-wave1,wave2,sflux = aurora.ssflux(iscale,f107,f107a,hlybr,fexvir,hlya,heiew,xuvfac)
-assert_allclose(sflux[[11,23]],(4.27225743e+11,   5.54400400e+07))
-#%% test rcolum
-""" VCD: Vertical Column Density """
-zcol,zvcd = aurora.rcolummod(sza,z*1e5,densd[['O','O2','N2']].values.T,tempd['heretemp'],nmaj)
-assert isclose(zcol[0,0], 1e30) #see rcolum comments for sun below horizon 1e30
-assert isclose(zvcd[2,5],8.04e+25,rtol=1e-2) #TODO changes a bit between python 2 / 3
+def test_snoem():
+    doy = datetime2gtd(dtime)[0]
+    zno,maglat,nozm = aurora.snoemmod.snoem(doy,1.75*log(0.4*ap),f107)
+    assert_allclose((nozm[12,15],nozm[-2,-1]),(33547142.,  44171752.))
+
+def test_fieldm():
+    xdip,ydip,zdip,totfield,dipang,decl,smodip = aurora.fieldm(glat,glon%360,z[50])
+    assert isclose(xdip,0.10698765516281128)
+    assert isclose(totfield,0.532055139541626)
+    assert isclose(dipang,76.86974334716797)
+
+def test_ssflux():
+    iscale=1; hlybr=0.; hlya=0.; fexvir=0.; heiew=0.; xuvfac=3.
+    wave1,wave2,sflux = aurora.ssflux(iscale,f107,f107a,hlybr,fexvir,hlya,heiew,xuvfac)
+    assert_allclose(sflux[[11,23]],(4.27225743e+11,   5.54400400e+07))
+
+def test_solzen_rcolum_snoemint_qback():
+    sza = aurora.szacalc(yd,utsec,glat,glon)
+    assert isclose(sza, 104.68412017822266)
+#%%
+ # (nighttime background ionization)
+    densd,tempd = rungtd1d(dtime,z,glat,glon,f107a,f107,[ap]*7)
+    znoint = aurora.snoemint(dtime.strftime('%Y%j'),glat,glon,f107,ap,z,tempd['heretemp'])
+    assert_allclose(znoint[[28,63]], (1.40939280e+08,   4.11383025e+06))
+    """ VCD: Vertical Column Density """
+    zcol,zvcd = aurora.rcolummod(sza,z*1e5,densd[['O','O2','N2']].values.T,tempd['heretemp'],nmaj)
+# FIXME these tests were numerically unstable (near infinity values)
+    assert isclose(zcol[0,0], 1e30) #see rcolum comments for sun below horizon 1e30
+    assert isclose(zvcd[2,5],8.04e+25,rtol=1e-2) #TODO changes a bit between python 2 / 3
 #%% skipping EPHOTO since we care about night time more for now
+#%%
+    #photoi = zeros((nst,nmaj,jmax),dtype=float32,order='F')
+    #phono = zeros((nst,jmax),dtype=float32,order='F')
+    photoi,phono = aurora.qback(zmaj=densd[['O','O2','N2']].values.T,zno=znoint,zvcd=zvcd,
+                         jm=jmax,nmaj=nmaj,nst=nst)
+def test_glow():
+    # electron precipitation
+    """ First enact "glow" subroutine, which calls QBACK, ETRANS and GCHEM among others 
+    """
+    aurora.glow() #no args
+    #aurora
 
-#%% test qback (nighttime background ionization)
-#photoi = zeros((nst,nmaj,jmax),dtype=float32,order='F')
-#phono = zeros((nst,jmax),dtype=float32,order='F')
-photoi,phono = aurora.qback(zmaj=densd[['O','O2','N2']].values.T,zno=znoint,zvcd=zvcd,
-                     jm=jmax,nmaj=nmaj,nst=nst)
-#%% electron precipitation
-""" First enact "glow" subroutine, which calls QBACK, ETRANS and GCHEM among others 
-"""
-aurora.glow() #no args
-#aurora
+    #%% ver and constituants
+    """
+    currently using common block CGLOW, in future use module
+    """
+    zceta = aurora.cglow.zceta.T
+    zeta = aurora.cglow.zeta.T[:,:11]
+    zcsum = zceta.sum(axis=-1)[:,:11]
+    assert_allclose(zcsum,zeta,rtol=1e-6)
 
-#%% ver and constituants
-"""
-currently using common block CGLOW, in future use module
-"""
-zceta = aurora.cglow.zceta.T
-zeta = aurora.cglow.zeta.T[:,:11]
-zcsum = zceta.sum(axis=-1)[:,:11]
-assert_allclose(zcsum,zeta,rtol=1e-6)
+if __name__ == '__main__':
+    test_egrid_maxt()
+    test_snoem()
+    test_fieldm()
+    test_ssflux()
+    test_solzen_rcolum_snoemint_qback()
+    test_glow()
