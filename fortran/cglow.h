5d05d4ca3bbd39557e870dca714bedef8e89f785
michael <hirsch617@gmail.com>
2015-09-21 02:25:36 -0400

    document nO+ monkey patch

--- fortran/cglow.h
+++ fortran/cglow.h
@@ -23,7 +23,7 @@
       real, parameter    :: G  = 978.1    !gravitational constant dynes
 
 ! Standard parameters for photoelectron or aurora runs (up to 50 keV):
-      integer, PARAMETER :: JMAX=120
+      integer, PARAMETER :: JMAX=128
       integer, PARAMETER :: NBINS=190
 !
 ! Parameters for high energy aurora (up to 100 MeV):
