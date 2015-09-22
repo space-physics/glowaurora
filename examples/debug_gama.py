ed4bc60d178b4948f8b17623227ad91d09b95676
scienceopen <scienceopen@users.noreply.github.com>
2015-09-22 18:35:03 -0400

    understanding issues from single float fortran code

---
+++ examples/debug_gama.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 17:49:18 2015
+
+@author: rollo
+"""
+from __future__ import division
+from numpy import float32
+
+Prodwnn1 = float32(-1.15867674E+09)
+PROdwn= float32(1.00000000E-30)
+Prodwnp1= float32(-640330880.)
+
+PROdup= float32(-998047680.)
+
+Prodn1= float32(0.)
+Prod= float32(1.00000000E-30)
+Prodp1= float32(0.)
+
+T1= float32(6.20506251E-12)
+T2= float32(2.29550547E-08)
+Alpha= float32(1.47421190E-06)
+Del2= float32(200000.000)
+
+
+gama = (
+        (Prod/float32(2)) * (-T1 - T2 - Alpha - (Prodp1 - Prodn1) /Prod / Del2)
+                  + PROdwn
+                  * ( -Alpha - T2 - (Prodwnp1 - Prodwnn1) /PROdwn / Del2 )
+                  - PROdup * T1
+       )
